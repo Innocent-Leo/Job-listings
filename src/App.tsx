f44704e237fb8e7304179e6e566161fd57318170
Innocent-Leo <innocentmaryleo@gmail.com>
2026-01-27 19:03:26 +0100

    feat: folder structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import FilterBar from "./components/FilterBar";
+// import Header from "./components/Header";
+// import JobList from "./components/JobList";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Header />
+      <FilterBar />
+      <JobList /> */}
+    </div>
+  );
+};
+
+export default App;
